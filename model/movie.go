093742ed3d987c66e21f26fd3a5717ae8154fe0b
Matteo Vaccari <vaccari@pobox.com>
2024-03-24 17:58:33 +0100

    Now retrieving real images from tmdb

--- model/movie.go
+++ model/movie.go
@@ -1,6 +1,7 @@
 package model
 
 type Movie struct {
-	Title    string `json:"title"`
-	Overview string `json:"overview"`
+	Title      string `json:"title"`
+	Overview   string `json:"overview"`
+	PosterPath string `json:"poster_path"`
 }
