05bb316120c6acd85d798de600c846d2b9ed6e33
Matteo Vaccari <vaccari@pobox.com>
2024-03-24 18:02:45 +0100

    Add dotenv

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/xpmatteo/gomovies/handlers"
 	"github.com/xpmatteo/gomovies/model"
 	"html/template"
@@ -14,7 +15,25 @@ import (
 
 const port = "8080"
 
+var data struct {
+	Results []model.Movie `json:"Results"`
+}
+
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+	readMovies()
+	fmt.Printf("Read %d movies\n", len(data.Results))
+	
+	templ := template.Must(template.ParseFiles("view/index.tmpl"))
+	http.Handle("GET /{$}", handlers.Index(templ, data.Results))
+	http.Handle("GET /", http.FileServer(http.Dir("./public/")))
+	log.Print("Serving HTTP from port ", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func readMovies() {
 	file, err := os.Open("movies.json")
 	if err != nil {
 		panic(err)
@@ -22,22 +41,11 @@ func main() {
 	defer file.Close()
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		panic(err)
-	}
-
-	var data struct {
-		Results []model.Movie `json:"Results"`
+		log.Fatal(err)
 	}
 
 	err = json.Unmarshal(bytes, &data)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatal("Error:", err)
 	}
-	fmt.Printf("Read %d movies\n", len(data.Results))
-	templ := template.Must(template.ParseFiles("view/index.tmpl"))
-	http.Handle("GET /{$}", handlers.Index(templ, data.Results))
-	http.Handle("GET /", http.FileServer(http.Dir("./public/")))
-	log.Print("Serving HTTP from port ", port)
-	log.Panic(http.ListenAndServe(":"+port, nil))
 }
